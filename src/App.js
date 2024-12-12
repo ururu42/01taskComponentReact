bf829909ddb76eff2b859806688986ae26520ab2
ururu42 <utepusechka@gmail.com>
2024-12-12 12:51:09 +0100

    выполнила Задание 1 Компоненты

--- src/App.js
+++ src/App.js
@@ -1,38 +1,94 @@
-import logo from './logo.svg'; // декларативный стиль
-import './App.css'; // декларативный стиль
-
-const getDate = () => {
-	// декларативный стиль
-	const today = new Date();
-	const month = today.getMonth() + 1;
-	const year = today.getFullYear();
-	const date = today.getDate();
-	return `${date}/${month}/${year}`;
-}
+import logo from './logo.svg';
+import styles from './App.module.css';
+import { useState } from 'react';
 
 export const App = () => {
-	const currentDate = getDate();
+	const [value, setValue] = useState('');
+
+	const [list, setList] = useState([]);
+
+	const [error, setError] = useState('');
+
+	const onInputButtonClick = () => {
+		const promptValue = prompt('Введите значение');
+		console.log(promptValue);
+		if (promptValue.length >= 3) {
+			setValue(promptValue);
+			setError('');
+		} else {
+			setError('Введенное значение должно быть не менее 3 символов');
+		}
+	};
+
+	let isValueValid = true;
+	if (value.length < 3) {
+		isValueValid = false;
+	}
+
+	const getDate = () => {
+		const today = new Date();
+		const month = today.getMonth() + 1;
+		const year = today.getFullYear();
+		const date = today.getDate();
+		const hours = today.getHours();
+		const minutes = today.getMinutes();
+		const seconds = today.getSeconds();
+		return `${date}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+	};
+
+	const onAddButtonClick = () => {
+		if (value.length >= 3) {
+			const date = getDate();
+
+			const updateList = [
+				...list,
+				{
+					id: Date.now(),
+					value: value,
+					date: date,
+				},
+			];
+			setList(updateList);
+			setValue('');
+			setError('');
+
+			console.log(list);
+		}
+	};
+
+	const pText = <p className={styles.noMarginText}>Нет добавленных элементов</p>;
 
 	return (
-		// декларативный стиль
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
+		<div className={styles.app}>
+			<h1 className={styles.pageHeading}>Ввод значения</h1>
+			<p className={styles.noMarginText}>
+				Текущее значение <code>value</code>: "
+				<output className={styles.currentValue}>{value}</output>"
+			</p>
+			{error !== '' ? <div className={styles.error}>{error}</div> : <div></div>}
+			<div className={styles.buttonsContainer}>
+				<button className={styles.button} onClick={onInputButtonClick}>
+					Ввести новое
+				</button>
+				<button
+					className={styles.button}
+					disabled={!isValueValid}
+					onClick={onAddButtonClick}
 				>
-					Learn React
-				</a>
-				<h2>{currentDate}</h2>
-			</header>
+					Добавить в список
+				</button>
+			</div>
+			<div className={styles.listContainer}>
+				<h2 className={styles.listHeading}>Список:</h2>
+				{list.length === 0 && pText}
+				<ul className={styles.list}>
+					{list.map(({ id, value, date }) => (
+						<li className={styles.listItem} key={id}>
+							{value} {date}
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
-
-
